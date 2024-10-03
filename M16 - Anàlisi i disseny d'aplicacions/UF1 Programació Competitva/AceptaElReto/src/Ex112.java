b141d71dfedac899dcea4b67e6e0b338347965ba
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2024-10-03 18:56:32 +0200

    Programame

---
+++ M16 - Anàlisi i disseny d'aplicacions/UF1 Programació Competitva/AceptaElReto/src/Ex112.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ex112 {
+	static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+                
+    }
+}
