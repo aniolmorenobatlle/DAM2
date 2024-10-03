19f4b2a7f84513347f418edf53a9b63aff63454d
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2024-10-03 18:56:32 +0200

    Programame

---
+++ M16 - Anàlisi i disseny d'aplicacions/UF1 Programació Competitva/AceptaElReto/src/Ex585.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Ex585 {
+    static Scanner sc = new Scanner(System.in);
+    static int casos;
+
+    public static void main(String[] args) throws Exception {
+        casos = sc.nextInt();
+
+        
+
+    }
+}
