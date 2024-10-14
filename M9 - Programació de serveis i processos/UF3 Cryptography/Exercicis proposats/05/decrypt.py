46f6c2dfca941fd80975c02429622a483f80396d
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2024-10-14 15:57:39 +0200

    Python A5

--- M9 - Programació de serveis i processos/UF3 Cryptography/Exercicis proposats/05/decrypt.py
+++ M9 - Programació de serveis i processos/UF3 Cryptography/Exercicis proposats/05/decrypt.py
@@ -8,17 +8,52 @@
 #       ● The number of vowels with respect to the total number of letters in the original message exceeds 37.00%.
 
 
-def decrypt():
-    word = "drobosckczisxpsvdbkdonkwyxqdrovsoedoxkxdc"
+import sys
+
+
+def decrypt(message):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
+    vocals = 'aeiou'
+    decrypted = None
     
-    # Provar tots els desplaçaments de l'alfabet(26 lletres)
-    for shift in range(26):
+    # Prova tots els possibles desplaçaments de 1 a 25, ja que l'abc hi han 26 lletres
+    for shift in range(1, 26):
+        text = ''
 
-        descrypted = ''
-        vocals = 0
+        # Important posar .lower que sino peta
+        for i in message.lower():
+            index = alphabet.index(i)
+            new_index = (index - shift) % 26
+            text += alphabet[new_index]
+        
+        vowel_count = 0
+        constant_count = 0
+
+
+        # Mirar si es vocal o consonant
+        for letter in text:
+            if letter in vocals:
+                vowel_count += 1
+            else:
+                constant_count += 1
+
+
+        # Calcular proporcio de vocals
+        vowel_ratio = vowel_count / (vowel_count + constant_count)
+        
+
+        # Mirar si està dins del rang de vocals
+        if 0.37 <= vowel_ratio <= 0.43 and (decrypted is None):
+            decrypted = text
+
+    return decrypted
 
 
 
+if len(sys.argv) > 1:
+    ciphertext = sys.argv[1]
 
-decrypt()
+    print(decrypt(ciphertext))
+
+else:
+    print("Please provide the function(decrypt or encrypt) and the message as a command line argument.")
