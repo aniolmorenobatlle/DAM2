d94dcb4eb9f80ac53758d18eeb707153e295c4d3
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2024-11-07 11:57:53 +0100

    JO-EL + Processos

---
+++ M8 - Programació multimedia/UF1 Desenvolupament d'aplicacions per a dispositius mòbils/Android Studio/Moreno_Aniol_Activitat11/app/src/main/java/dam2/amoreno/moreno_aniol_activitat11/classes/Products.kt
@@ -0,0 +1,8 @@
+package dam2.amoreno.moreno_aniol_activitat11.classes
+
+data class Products(
+    val marca: String,
+    val model: String,
+    val quantitat: String,
+    val nomImatge: String
+)
