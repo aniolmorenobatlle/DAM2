2126b8a2380c2a3452231448795417e399e06ef6
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2024-11-27 15:32:23 +0100

    Manage Bank Account

--- M9 - Programació de serveis i processos/UF1 Processes and Threads/Exercicis proposats/manage_bank_account/manage_bank_account.py
+++ M9 - Programació de serveis i processos/UF1 Processes and Threads/Exercicis proposats/manage_bank_account/manage_bank_account.py
@@ -1,28 +1,39 @@
-# manage_bank_account.py
+# 05. The following program written in Python sometimes does not return a correct result.
+# Modify it with some multiprogramming synchronization mechanism so that it returns always the correct result.
+
+
 import multiprocessing as mp
 import time
 
-def deposit(b, t):
-    for u in range(t):
+def deposit(b, t, lock):
+    for _ in range(t):
         time.sleep(0.01)
-        b.value += 5
+
+        with lock:
+            b.value += 5
 
 
-def withdraw(b, t):
-    for w in range(t):
+def withdraw(b, t, lock):
+    for _ in range(t):
         time.sleep(0.01)
-        b.value -= 5
+
+        with lock:
+            b.value -= 5
 
 
 
 if __name__ == '__main__':
 
-    balance = mp.Value('i', 500)
-    process_deposit = mp.Process(target=deposit, args=(balance, 100))
-    process_withdraw = mp.Process(target=withdraw, args=(balance, 100))
+    balance = mp.Value('i', 500) # Començar a 500€
+    lock = mp.Lock()
+
+    process_deposit = mp.Process(target=deposit, args=(balance, 100, lock))
+    process_withdraw = mp.Process(target=withdraw, args=(balance, 100, lock))
+
     process_deposit.start()
     process_withdraw.start()
+    
     process_deposit.join()
     process_withdraw.join()
-    
+
     print(balance.value)
