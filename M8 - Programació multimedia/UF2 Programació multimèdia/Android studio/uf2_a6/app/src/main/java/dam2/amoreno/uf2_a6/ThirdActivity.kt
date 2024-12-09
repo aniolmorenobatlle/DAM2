51ee5f58c2cea8a366cf096392f75fb6a8c57638
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2024-12-09 20:26:45 +0100

    Acabar M8 UF2 A6 + Zip M8 UF2 A4

--- M8 - Programació multimedia/UF2 Programació multimèdia/Android studio/uf2_a6/app/src/main/java/dam2/amoreno/uf2_a6/ThirdActivity.kt
+++ M8 - Programació multimedia/UF2 Programació multimèdia/Android studio/uf2_a6/app/src/main/java/dam2/amoreno/uf2_a6/ThirdActivity.kt
@@ -1,10 +1,7 @@
 package dam2.amoreno.uf2_a6
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
