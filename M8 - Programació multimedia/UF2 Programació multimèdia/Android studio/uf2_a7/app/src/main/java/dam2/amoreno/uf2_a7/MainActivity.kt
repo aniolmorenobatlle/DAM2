4d522402e8d0b16b14193e7621798b46c9564c53
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2025-01-08 17:04:57 +0100

    UF2 A7

---
+++ M8 - Programació multimedia/UF2 Programació multimèdia/Android studio/uf2_a7/app/src/main/java/dam2/amoreno/uf2_a7/MainActivity.kt
@@ -0,0 +1,11 @@
+package dam2.amoreno.uf2_a7
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ M9 - Programació de serveis i processos/UF1 Processes and Threads/Exercicis proposats/Threads/square/square.py
@@ -0,0 +1,2 @@
+# 03. Create a Python program (square.py) that draws a square, to do this it will first ask the length of the side and then draw it (curses) using a thread (holding the drawing on the screen for five seconds)
+
