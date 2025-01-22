be2affbcb4985cf01a0ae17dab2f5442f2316932
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2025-01-22 14:35:25 +0100

    M8 UF3 Notificacions app

--- M8 - Programació multimedia/UF3 Unity/Android Studio/exemple_notis/app/src/main/java/dam2/amoreno/exemple_notis/FirebaseNotification.kt
+++ M8 - Programació multimedia/UF3 Unity/Android Studio/exemple_notis/app/src/main/java/dam2/amoreno/exemple_notis/FirebaseNotification.kt
@@ -1,4 +1,37 @@
 package dam2.amoreno.exemple_notis
 
-class FirebaseNotification {
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.remoteMessage
+
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class FirebaseNotification : FirebaseMessagingService() {
+    private val CHANNEL_ID = "firebase_notifications"
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentTitle(message.notification?.title)
+            .setContentText(message.notification?.body)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        NotificationManagerCompat.from(this).notify(1, notification)
+    }
 }
---
+++ M8 - Programació multimedia/UF3 Unity/Android Studio/exemple_notis/app/src/main/java/dam2/amoreno/exemple_notis/MainActivity.kt
@@ -0,0 +1,11 @@
+package dam2.amoreno.exemple_notis
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
