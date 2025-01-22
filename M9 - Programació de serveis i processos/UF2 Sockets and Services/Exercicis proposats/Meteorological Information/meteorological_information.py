2893bd9ce94c5e68fdbe0eb59c0c15f8dd3d169e
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2025-01-22 16:25:36 +0100

    M9 UF2 E1

--- M9 - Programació de serveis i processos/UF2 Sockets and Services/Exercicis proposats/Meteorological Information/meteorological_information.py
+++ M9 - Programació de serveis i processos/UF2 Sockets and Services/Exercicis proposats/Meteorological Information/meteorological_information.py
@@ -1,75 +1,85 @@
 import http.server
-import re
 import socketserver
-import urllib.request
+import time
+
+import requests
 
 citys = ["barcelona", "girona", "tarragona", "lleida", "palamos"]
+api_key = "9b25e0adb03146d6a5b144231252201"
 
 def get_weather():
-    city_temperatures = []
+    city_data = []
 
     for city in citys:
         try:
-            url = f"https://www.eltiempo.es/{city}.html"
-            data = urllib.request.urlopen(url)
-            charset = data.headers.get_content_charset()
-            html = data.read().decode(charset)
+            url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no"
+            response = requests.get(url)
+            data = response.json()
+
+            temperature = data['current']['temp_c']
+            humidity = data['current']['humidity']
+            wind_speed_kmh = data['current']['wind_kph']
+
+            city_data.append((city.capitalize(), temperature, humidity, wind_speed_kmh))
+            
+        except Exception:
+            city_data.append((city.capitalize(), "Error", "Error"))
+
+    return city_data
 
 
-            if "'currentTemperature':'" in html:
-                part1 = html.split("'currentTemperature':'")[1]  # Agafar primer part despres de currentTemperature (que sera el nombre)
-                temperature = part1.split("'")[0]
-                city_temperatures.append((city.capitalize(), temperature))
-            else:
-                city_temperatures.append((city.capitalize(), "N/A"))
-
-
-        except Exception as e:
-            city_temperatures.append((city.capitalize(), "Error"))
-
-    return city_temperatures
-
-def update_html(city_temperatures):
+def update_html(city_data):
     html_template = """
-    <html>
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Weather Report</title>
-        </head>
-        <body>
-            <h1>Weather Report</h1>
-            <ul>
-                {content}
-            </ul>
-        </body>
-    </html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Weather Report</title>
+    </head>
+    <body>
+        <h1>Weather Report</h1>
+        <ul>
+            {content}
+        </ul>
+    </body>
+</html>
     """
 
     content = ""
-    
-    for city, temperature in city_temperatures:
-        content += f"<li>{city}: {temperature}</li>\n"
+    for city, temperature, humidity, wind_speed in city_data:
+        content += f"""
+            <li>
+                <span style='font-size:20px; font-weight: bold;'>{city}</span>
+                <ul>
+                    <li>Temperatura: { temperature }°C</li>
+                    <li>Humitat: { humidity }%</li>
+                    <li>Velocitat del vent: { wind_speed } km/h</li>
+                </ul>
+            </li>
+        """
 
-    final_html = html_template.format(content = content)
+    final_html = html_template.format(content=content)
 
     with open("index.html", "w", encoding="utf-8") as f:
         f.write(final_html)
 
 
-def run_server() :
+def run_server():
     address = ('127.0.0.1', 8000)
     handler = http.server.SimpleHTTPRequestHandler
     httpd = socketserver.TCPServer(address, handler)
 
-    print(f"serving on port {address[1]}")
+    print(f"Serving on port {address[1]}")
 
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         print("Interrupted!")
 
+
 if __name__ == '__main__':
-    city_temperatures = get_weather()
-    update_html(city_temperatures)
+    print("Starting server...\n")
+
+    city_data = get_weather()
+    update_html(city_data)
     run_server()
